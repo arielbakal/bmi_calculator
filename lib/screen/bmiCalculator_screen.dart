cbc16cb9ec117198624b9d0299764c023e62733a
arielbakal <arielbakal.dev@gmail.com>
2022-05-27 11:24:47 -0300

    Created /screen/bmiCalculator_screen.dart

---
+++ lib/screen/bmiCalculator_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculatorScreen extends StatefulWidget{
+  const BmiCalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BmiCalculatorScreenState();
+
+}
+
+class _BmiCalculatorScreenState extends State<BmiCalculatorScreen>{
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
