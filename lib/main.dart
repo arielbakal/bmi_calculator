958a8c5d52e1291ac0e4bfd67d8ee608aa60501d
arielbakal <arielbakal.dev@gmail.com>
2022-05-27 11:28:22 -0300

    imported bmiCalculator_screen.dart on main.dart, then make it home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/screen/bmiCalculator_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: BmiCalculatorScreen(),
     );
   }
 }
