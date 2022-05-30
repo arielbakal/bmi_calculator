02602ecc1f0e01bed8f76ce98ab520dc97e79c96
arielbakal <arielbakal.dev@gmail.com>
2022-05-30 20:00:05 -0300

    Code cleaning

--- lib/screen/bmiCalculator_screen.dart
+++ lib/screen/bmiCalculator_screen.dart
@@ -1,3 +1,4 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 class BmiCalculatorScreen extends StatefulWidget {
@@ -8,14 +9,13 @@ class BmiCalculatorScreen extends StatefulWidget {
 }
 
 class _BmiCalculatorScreenState extends State<BmiCalculatorScreen> {
-
   final TextEditingController _heightController = TextEditingController();
   final TextEditingController _weightController = TextEditingController();
 
   double _result = 0;
 
-  void calculateBMI(){
-    double height = double.parse(_heightController.text) / 100 ;
+  void calculateBMI() {
+    double height = double.parse(_heightController.text) / 100;
 
     double weight = double.parse(_weightController.text);
 
@@ -25,11 +25,9 @@ class _BmiCalculatorScreenState extends State<BmiCalculatorScreen> {
 
     _result = result;
     setState(() {});
-
   }
 
-  void reset(){
-
+  void reset() {
     _result = 0;
     setState(() {});
   }
@@ -82,10 +80,9 @@ class _BmiCalculatorScreenState extends State<BmiCalculatorScreen> {
               Text(
                 _result.toStringAsFixed(2),
                 style: const TextStyle(
-                  color: Colors.amber,
-                  fontSize: 19.4,
-                  fontWeight: FontWeight.w500
-                ),  
+                    color: Colors.amber,
+                    fontSize: 19.4,
+                    fontWeight: FontWeight.w500),
               ),
             ],
           ),
